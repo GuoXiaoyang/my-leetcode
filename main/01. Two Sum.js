3c8262d8cf45f0234afd72353760fd7e93b5dc15
guoxiaoyang <guoxiaoyang0925@qq.com>
2017-03-19 23:36:19 +0800

    submit No.2 Problem

---
+++ main/01. Two Sum.js
@@ -0,0 +1,34 @@
+/**
+ * Created by gxy on 2017/3/4.
+ */
+
+/* leetcode_array.js
+ 01. 2Sum
+ Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+ You may assume that each input would have exactly one solution, and you may not use the same element twice.
+ */
+var twoSum = function (nums, target) {
+    if (nums.length < 2) return;
+    var res;
+    var numPosHash = {};
+    for (var i = 0; i < nums.length; i++) {
+        // console.log("numPosHash:", numPosHash);
+        // console.log("numPosHash[(target-nums[i]).toString():", numPosHash[(target-nums[i]).toString()]);
+        if (numPosHash[(target - nums[i]).toString()] === undefined) {
+            numPosHash[nums[i].toString()] = i;
+        } else {
+            var pos = parseInt(numPosHash[(target - nums[i]).toString()]);
+            res = [pos, i];
+            break;
+        }
+    }
+    return res;
+};
+//test
+/*var testArray1 = [1,7,2,8,5,6];
+ var result1 = twoSum(testArray1, 13);
+ console.log("result1:", result1);*/
+
+
+
---
+++ main/02.Add Two Numbers.js
@@ -0,0 +1,68 @@
+/**
+ * Created by gxy on 2017/3/19.
+ */
+/**
+ * 02. Add Two Numbers
+ * You are given two non-empty linked lists representing two non-negative integers.
+ * The digits are stored in reverse order and each of their nodes contain a single digit.
+ * Add the two numbers and return it as a linked list.
+
+ * You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+ Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+ Output: 7 -> 0 -> 8
+
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+function ListNode(val) {
+    this.val = val;
+    this.next = null;
+}
+
+var addTwoNumbers = function (l1, l2) {
+    var p = l1, q = l2;
+    var pHead = new ListNode(0);
+    var r = pHead;
+    var temp;
+    var flag = 0;
+    while (p || q || flag) {
+        // console.log(p.val,"&",q.val);
+        var sum = parseInt((p ? p.val : 0)) + parseInt((q ? q.val : 0)) + flag;
+        // console.log(sum);
+        p = p ? p.next : p;
+        q = q ? q.next : q;
+        temp = new ListNode(parseInt(sum % 10));
+        flag = parseInt(sum / 10);
+        r.next = temp;
+        r = r.next;
+
+    }
+    return pHead.next;
+};
+
+
+// test
+var ph = new ListNode();
+var rh = ph;
+var tp;
+for (var i = 1; i < 10; i++) {
+    // rh.val = i;
+    tp = new ListNode(i);
+    rh.next = tp;
+    rh = rh.next;
+}
+var l = ph.next;
+/*while (l) {
+ console.log(l.val, "->");
+ l = l.next;
+ }*/
+
+var res = addTwoNumbers(l, l);
+while (res) {
+    console.log(res.val, "->");
+    res = res.next;
+}
