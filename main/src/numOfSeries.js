6adebf6f1daa47ecbc5d8b9ade13fb72b5bf20f9
guoxiaoyang <guoxiaoyang0925@qq.com>
2017-08-13 19:34:06 +0800

    --amend

--- main/src/numOfSeries.js
+++ main/src/numOfSeries.js
@@ -30,8 +30,66 @@
  * @param {number} k
  * @return {number} value
  */
-var numOfSeries = function (n, k) {
+const numOfSeries = function (n, k) {
+  // if(k===1) return 1;
+  // if(n===1) return k;
+  // get factors of nums <= k
+  const factors = [[]];
+  for (let i = 2; i <= k; i += 1) {
+    factors[i - 1] = getFactor(i);
+  }
+  // elements with [factor[i][j],n-1]
+  let nums = new Array(k).fill(1);
+  // nums = nums.map((v, i) => factors[i].length);
+  // f(1,k)
+  let sumNum = k;
+  for (let i = 1; i < n; i += 1) {
+    // get f(i,k)
+    // let tmpNums = new Array(k).fill(sumNum);
+    let tmpNums;
+    if(nums.every(v => v > 1000000007)) {
+      nums = nums.map(v => v%1000000007);
+      tmpNums = new Array(k).fill(sumNum%1000000007);
+    } else {
+      tmpNums = new Array(k).fill(sumNum);
+    }
+    for (let j = 1; j < k; j += 1) {
+      // get w(factor(k),i)
+      for (let l = 0; l < factors[j].length; l += 1) {
 
+        tmpNums[j] -= nums[factors[j][l]-1];
+      }
+    }
+    sumNum = tmpNums.reduce((v1, v2) => v1 + v2);
+    nums = tmpNums.slice();
+  }
+  return sumNum%1000000007;
 };
 
+function getFactor(num) {
+  const factors = [1];
+  for (let i = 2; i < num; i += 1) {
+    if (num % i === 0) factors.push(i);
+  }
+  return factors;
+}
+
+
 // test
+const n = 5;
+const k = 10000;
+console.time('time');
+const res = numOfSeries(n, k);
+console.timeEnd('time');
+console.log('res:', res);
+
+// log
+// n:3,k:3
+// ans: 15
+// times: 5.827ms
+
+// n:5,k:1000
+// ans: 104446767
+// times: 69.312ms
+
+
