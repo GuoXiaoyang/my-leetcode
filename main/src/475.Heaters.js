34e3fbb255bd4d0160bad1cda6e448bd4cb576db
guoxiaoyang <guoxiaoyang0925@qq.com>
2017-08-13 17:37:07 +0800

    add NetEase programming solution

--- main/src/475.Heaters.js
+++ main/src/475.Heaters.js
@@ -3,11 +3,14 @@
  */
 /****************************************************************
  475. Heaters
- Winter is coming! Your first job during the contest is to design a standard heater with fixed warm radius to warm all the houses.
+ Winter is coming! Your first job during the contest is to design a standard heater with fixed
+ warm radius to warm all the houses.
 
- Now, you are given positions of houses and heaters on a horizontal line, find out minimum radius of heaters so that all houses could be covered by those heaters.
+ Now, you are given positions of houses and heaters on a horizontal line, find out minimum radius
+ of heaters so that all houses could be covered by those heaters.
 
- So, your input will be the positions of houses and heaters seperately, and your expected output will be the minimum radius standard of heaters.
+ So, your input will be the positions of houses and heaters seperately, and your expected
+ output will be the minimum radius standard of heaters.
 
  Note:
  Numbers of houses and heaters you are given are non-negative and will not exceed 25000.
@@ -17,7 +20,8 @@
  Example 1:
  Input: [1,2,3],[2]
  Output: 1
- Explanation: The only heater was placed in the position 2, and if we use the radius 1 standard, then all the houses can be warmed.
+ Explanation: The only heater was placed in the position 2, and if we use the radius 1 standard,
+ then all the houses can be warmed.
  Example 2:
  Input: [1,2,3,4],[1,4]
  Output: 1
